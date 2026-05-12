1c338105b2dec8c8a17a8f92d2a86b8477e23a00
Srinithin Somasundaram <srinithinoffl@gmail.com>
2026-05-12 18:00:54 +0530

    Support split MongoDB env configuration

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,41 @@ const mongoose = require("mongoose");
 const { env } = require("./env");
 const { logger } = require("./logger");
 
+function buildMongoUri() {
+  if (env.mongodbUri) {
+    return env.mongodbUri;
+  }
+
+  if (!env.mongodbHost) {
+    return "";
+  }
+
+  const scheme = env.mongodbScheme === "mongodb+srv" ? "mongodb+srv" : "mongodb";
+  const databaseName = env.mongodbDatabase || "sprintview";
+  const credentials =
+    env.mongodbUsername && env.mongodbPassword
+      ? `${encodeURIComponent(env.mongodbUsername)}:${encodeURIComponent(env.mongodbPassword)}@`
+      : "";
+  const hostPort = scheme === "mongodb+srv" ? env.mongodbHost : `${env.mongodbHost}:${env.mongodbPort}`;
+  const query = new URLSearchParams();
+
+  if (env.mongodbAuthSource) {
+    query.set("authSource", env.mongodbAuthSource);
+  }
+
+  const queryString = query.toString();
+  return `${scheme}://${credentials}${hostPort}/${databaseName}${queryString ? `?${queryString}` : ""}`;
+}
+
 async function connectDatabase() {
-  if (!env.mongodbUri) {
-    throw new Error("MONGODB_URI is required");
+  const mongodbUri = buildMongoUri();
+
+  if (!mongodbUri) {
+    throw new Error("MONGODB_URI or MONGODB_HOST is required");
   }
 
   mongoose.set("strictQuery", true);
-  await mongoose.connect(env.mongodbUri);
+  await mongoose.connect(mongodbUri);
   logger.info("MongoDB connected");
 }
 
